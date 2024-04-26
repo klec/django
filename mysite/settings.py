6327dbf6f95ff7d7ee80fa98122794908380d65e
Yuri Kletsun <klechome@gmail.com>
2024-04-26 15:40:10 +0300

    add logger

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,19 @@ MEDIA_ROOT = '/home/klec/mysite/media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/klec/mysite/static'
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'debug.log'),
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,14 @@
+import logging
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
+logger = logging.getLogger(__name__)
 
 def index(request):
+    soap_request_data = request.body
+
+    # Залогируйте данные SOAP-запроса
+    logger.info('Received SOAP request: %s', soap_request_data)
     return HttpResponse("Hello, world. You're at the polls index.")
